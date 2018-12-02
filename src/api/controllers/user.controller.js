43182c190da4156a83daa32638a3049b70799e06
Oren Zomer <oren@awearsolutions.com>
2018-12-03 00:43:58 +0200

    calling next with error instead of calling errorHandler directly

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,7 +1,6 @@
 const httpStatus = require('http-status');
 const { omit } = require('lodash');
 const User = require('../models/user.model');
-const { handler: errorHandler } = require('../middlewares/error');
 
 /**
  * Load user and append to req.
@@ -13,7 +12,7 @@ exports.load = async (req, res, next, id) => {
     req.locals = { user };
     return next();
   } catch (error) {
-    return errorHandler(error, req, res);
+    return next(error);
   }
 };
 
