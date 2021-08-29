eb50c0ef713b2d2409d04083a87342b86c675f9f
Daniel Sousa <sousa.dfs@gmail.com>
2021-08-29 13:25:58 -0300

    fix dotenv-safe method name

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 // import .env variables
-require('dotenv-safe').load({
+require('dotenv-safe').config({
   path: path.join(__dirname, '../../.env'),
   sample: path.join(__dirname, '../../.env.example'),
 });
