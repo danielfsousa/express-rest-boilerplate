6dfde84f4e11f78cf250ed98459a2eee602fa664
Daniel Sousa <sousa.dfs@gmail.com>
2021-10-11 17:08:29 -0300

    fix dotenv-safe example option (#512)

--- src/config/vars.js
+++ src/config/vars.js
@@ -3,7 +3,7 @@ const path = require('path');
 // import .env variables
 require('dotenv-safe').config({
   path: path.join(__dirname, '../../.env'),
-  sample: path.join(__dirname, '../../.env.example'),
+  example: path.join(__dirname, '../../.env.example'),
 });
 
 module.exports = {
