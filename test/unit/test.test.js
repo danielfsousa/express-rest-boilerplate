d995f734616afa8ff8f3568a264c6ec789ba934e
Daniel Sousa <sousa.dfs@gmail.com>
2022-10-29 16:58:00 -0300

    ci: test

--- test/unit/test.test.js
+++ test/unit/test.test.js
@@ -1,5 +1,10 @@
-import { expect, test } from 'vitest'
+import { MongoDBContainer } from 'testcontainers'
+import { test } from 'vitest'
+import * as database from '#lib/database'
 
-test('testing ci', () => {
-  expect(1 + 1).toBe(2)
+test('testing ci', async () => {
+  const mongodbContainer = await new MongoDBContainer('mongo:6').start()
+  await database.connect(mongodbContainer.getConnectionString())
+  await mongodbContainer.stop()
+  console.log('connected to db')
 })
