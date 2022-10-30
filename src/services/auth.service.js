89069cd1f4f87652e15c888da2e5876d50e4e9c2
Daniel Sousa <sousa.dfs@gmail.com>
2022-10-29 21:38:05 -0300

    chore: replace axios with got (#635)

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,11 @@
-// TODO: use got
-import axios from 'axios'
+import got from 'got'
 
 export async function facebook(token) {
   const fields = 'id, name, email, picture'
   const url = 'https://graph.facebook.com/me'
-  const params = { access_token: token, fields }
-  const response = await axios.get(url, { params })
-  const { id, name, email, picture } = response.data
+  const searchParams = { access_token: token, fields }
+  const response = await got.get(url, { searchParams }).json()
+  const { id, name, email, picture } = response
   return {
     service: 'facebook',
     picture: picture.data.url,
@@ -18,9 +17,9 @@ export async function facebook(token) {
 
 export async function google(token) {
   const url = 'https://www.googleapis.com/oauth2/v3/userinfo'
-  const params = { access_token: token }
-  const response = await axios.get(url, { params })
-  const { sub, name, email, picture } = response.data
+  const searchParams = { access_token: token }
+  const response = await got.get(url, { searchParams }).json()
+  const { sub, name, email, picture } = response
   return {
     service: 'google',
     picture,
